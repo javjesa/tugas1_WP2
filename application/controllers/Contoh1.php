e572a05fd0457aeab1ce78243549c5409829d044
javjesa <javaekasabdaalam@gmail.com>
2023-03-31 14:29:42 +0700

    TUGAS1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"JAVA EKA SABDA ALAM
+
+        NIM : 17200391
+        Kelas : 17.6A.07
+        ASAL J A K A R T A";
+    }
+}
